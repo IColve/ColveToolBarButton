45dbd57491942f89df4262511ed8654dcb3b13e3
Colve <573582096@qq.com>
2019-10-21 11:34:36 +0800

    feat: 增加工具栏显示自定义按钮

---
+++ ColveToolBarButton/Assets/ToolBarButtonEditor/ToolBarButtonEditor.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+using System;
+
+public class ToolBarButtonEditor : MonoBehaviour 
+{
+	private static Rect leftButtonRect = new Rect(380, 5, 110, 22);
+	private static Rect rightButtonRect = new Rect(1050, 5, 110, 22);
+
+	private static void OnGUI()
+	{
+		List<ToolBarButtonClass> buttonList = ToolBarButtonManager.ToolBarButtonList;
+
+		for (int i = 0; i < buttonList.Count; i++)
+		{
+			Rect _buttonRect = GetRect(i + 1);
+			if (GUI.Button(_buttonRect, buttonList[i].name, new GUIStyle("button")))
+			{
+				buttonList[i].action();
+			}
+
+			_buttonRect.x += _buttonRect.width + 1;
+		}
+	}
+
+	private static Rect GetRect(int index)
+	{
+		if (index <= 4)
+		{
+			float x = (index - 1) * (leftButtonRect.width + 1) + leftButtonRect.x;
+			return new Rect(x, leftButtonRect.y, leftButtonRect.width, leftButtonRect.height);
+		}
+		else
+		{
+			float x = (index - 5) * (rightButtonRect.width + 1) + rightButtonRect.x;
+			return new Rect(x, rightButtonRect.y, rightButtonRect.width, rightButtonRect.height);
+		}
+	}
+	
+	[InitializeOnLoadMethod]
+	private static void InitializeOnLoad()
+	{
+		EditorApplication.delayCall += WaitForUnityEditorToolbar;
+	}
+
+	private static Type Toolbar =
+		typeof(EditorGUI)
+			.Assembly
+			.GetType("UnityEditor.Toolbar");
+
+	private static FieldInfo Toolbar_get =
+		Toolbar
+			.GetField("get");
+
+	private static Type GUIView =
+		typeof(EditorGUI)
+			.Assembly
+			.GetType("UnityEditor.GUIView");
+
+	private static PropertyInfo GUIView_imguiContainer =
+		GUIView
+			.GetProperty(
+				"imguiContainer",
+				BindingFlags.Instance |
+				BindingFlags.Public |
+				BindingFlags.NonPublic);
+
+	private static FieldInfo IMGUIContainer_m_OnGUIHandler =
+		typeof(IMGUIContainer)
+			.GetField(
+				"m_OnGUIHandler",
+				BindingFlags.Instance |
+				BindingFlags.Public |
+				BindingFlags.NonPublic);
+
+	private static void WaitForUnityEditorToolbar()
+	{
+		var toolbar = Toolbar_get.GetValue(null);
+		if (toolbar != null)
+		{
+			AttachToUnityEditorToolbar(toolbar);
+			return;
+		}
+
+		EditorApplication.delayCall += WaitForUnityEditorToolbar;
+	}
+
+	private static void AttachToUnityEditorToolbar(object toolbar)
+	{
+		var toolbarGUIContainer = (IMGUIContainer) GUIView_imguiContainer.GetValue(toolbar, null);
+
+		var toolbarGUIHandler =
+			(Action)
+			IMGUIContainer_m_OnGUIHandler
+				.GetValue(toolbarGUIContainer);
+
+		toolbarGUIHandler += OnGUI;
+
+		IMGUIContainer_m_OnGUIHandler
+			.SetValue(toolbarGUIContainer, toolbarGUIHandler);
+	}
+}
---
+++ ColveToolBarButton/Assets/ToolBarButtonEditor/ToolBarButtonManager.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ToolBarButtonManager : MonoBehaviour
+{
+	private static List<ToolBarButtonClass> toolBarButtonList;
+
+	public static List<ToolBarButtonClass> ToolBarButtonList
+	{
+		get
+		{
+			UpdateButtonList();
+			return toolBarButtonList;
+		}
+	}
+
+	private static void UpdateButtonList()
+	{
+		if (toolBarButtonList == null)
+		{
+			toolBarButtonList = new List<ToolBarButtonClass>();
+		}
+		
+		toolBarButtonList.Clear();
+
+		UpdateAddButton();
+
+		toolBarButtonList.RemoveAll(x => string.IsNullOrEmpty(x.name));
+	}
+
+	private static void UpdateAddButton()
+	{
+		toolBarButtonList.Add(new ToolBarButtonClass(button1Name, Button1Action));
+		toolBarButtonList.Add(new ToolBarButtonClass(button2Name, Button2Action));
+		toolBarButtonList.Add(new ToolBarButtonClass(button3Name, Button3Action));
+		toolBarButtonList.Add(new ToolBarButtonClass(button4Name, Button4Action));
+		toolBarButtonList.Add(new ToolBarButtonClass(button5Name, Button5Action));
+		toolBarButtonList.Add(new ToolBarButtonClass(button6Name, Button6Action));
+		toolBarButtonList.Add(new ToolBarButtonClass(button7Name, Button7Action));
+		toolBarButtonList.Add(new ToolBarButtonClass(button8Name, Button8Action));
+	}
+
+	//Edit toolbar here
+	//if name is empty, never show on toolbar;
+	#region ButtonAction
+
+	private static string button1Name = "1";
+	private static void Button1Action()
+	{
+		Debug.Log(1);
+	}
+	
+	private static string button2Name = "";
+	private static void Button2Action()
+	{
+		
+	}
+	
+	private static string button3Name = "";
+	private static void Button3Action()
+	{
+		
+	}
+	
+	private static string button4Name = "";
+	private static void Button4Action()
+	{
+		
+	}
+	
+	private static string button5Name = "";
+	private static void Button5Action()
+	{
+		
+	}
+	
+	private static string button6Name = "6";
+	private static void Button6Action()
+	{
+		Debug.Log(6);
+	}
+	
+	private static string button7Name = "";
+	private static void Button7Action()
+	{
+		
+	}
+	
+	private static string button8Name = "8";
+	private static void Button8Action()
+	{
+		Debug.Log(8);
+	}
+
+	#endregion
+	
+
+}
+
+public class ToolBarButtonClass
+{
+	public string name;
+	public Action action;
+
+	public ToolBarButtonClass(string name, Action action)
+	{
+		this.name = name;
+		this.action = action;
+	}
+}
